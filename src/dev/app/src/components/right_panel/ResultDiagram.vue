9f08b30928d68edd7c360a96b392938ce040297c
maximivanov2004 <maxim.ivanov1711@gmail.com>
2021-03-26 22:27:54 +0300

    Added result diagram interface

--- src/dev/app/src/components/right_panel/ResultDiagram.vue
+++ src/dev/app/src/components/right_panel/ResultDiagram.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="result-diagram">result: {{ percent }}%</div>
+  <div class="result-diagram">
+    <div
+      class="percent"
+      :class="{
+        yellow: percent > 40 && percent < 70,
+        red: percent < 40,
+      }"
+    >
+      {{ percent }}%
+    </div>
+    <img
+      class="diagram"
+      :class="{
+        yellow: percent > 40 && percent < 70,
+        red: percent < 40,
+      }"
+      src="../../assets/icons/computer_case.png"
+      alt="diagram"
+    />
+  </div>
 </template>
 
 <script>
@@ -15,3 +34,38 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.result-diagram {
+  height: 420px;
+  display: flex;
+  align-items: center;
+}
+
+.percent {
+  margin-left: 20px;
+  font-size: 50px;
+  color: var(--main-green);
+}
+.percent.yellow {
+  color: var(--yellow);
+}
+.percent.red {
+  color: var(--red);
+}
+
+.diagram {
+  margin-left: auto;
+  margin-right: 20px;
+  padding: 40px;
+  height: 270px;
+  border: 14px solid var(--main-green);
+  border-radius: 250px;
+}
+.diagram.yellow {
+  border-color: var(--yellow);
+}
+.diagram.red {
+  border-color: var(--red);
+}
+</style>
